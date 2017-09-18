b9b145691f8c7fe66a68950c864b7da0e038f279
Paul <pjruess@gmail.com>
2017-09-17 19:29:24 -0500

    Added first scripts for homework 1, problems 2 and 3

--- fall2017/stoch_hydro/r_scripts/hw1/hw1_p2.r
+++ fall2017/stoch_hydro/r_scripts/hw1/hw1_p2.r
@@ -1,11 +1,41 @@
 # Start PDF plotting device
-pdf("hw1_p2.pdf", height=5, width=5)
+pdf('hw1_p2.pdf', height=5, width=5)
 
-usgs.disch <- read.csv("usgs_08074000_daily_streamflow.csv")
+usgs.disch <- read.csv('usgs_08068720_daily_streamflow.csv')
 
-plot(usgs.disch$datetime,usgs.disch$streamflow_cfs)
+plot(usgs.disch$date,usgs.disch$flow_cfs,
+	xlab='Date',ylab='Daily Mean Streamflow (CFS)',
+	# xaxt='n',#yaxt='n',
+	main='Daily Mean Streamflow at Brays Bayou,\n Houston, TX from 9/17/2016 - 9/16/2017')
 
-hist(usgs.disch$streamflow_cfs)
+# xts package saved to /tmp/RtmpCnm0nY/downloaded_packages
+
+# Insert x-axis (axis 1) ticks and title
+# axis(1, seq(min(usgs.disch$Date), max(usgs.disch$Date), 12), las=2) # years by fives, vertical label
+# ticks <- axTicksByTime(usgs.disch$Date,'months',format.labels='%b-%Y')
+# axis(1,at = .index(usgs.disch$Date)[ticks], labels = names(ticks),mgp=c(0,0.5,0))
+# title(xlab="Date")#, cex.lab=1.25) # increase font size
+
+hist(usgs.disch$flow_cfs,
+	xlab='Daily Mean Streamflow (CFS)',
+	prob=FALSE,breaks=100)
+
+# Is this mean correct? Ignores NA values
+mean <- mean(usgs.disch$flow_cfs,na.rm=TRUE)
+stdev <- sd(usgs.disch$flow_cfs,na.rm=TRUE)
+n <- length(usgs.disch$flow_cfs[!is.na(usgs.disch$flow_cfs)]) # Ignore NA values
+error <- qnorm(0.975)*stdev/sqrt(n)
+l <- mean-error
+r <- mean+error
+
+# Report data
+# plot(0:10, type = "n", xaxt="n", yaxt="n", bty="n", xlab = "", ylab = "")
+text(5,8,cat('Mean: ', mean, '\n'))
+text(5,7,cat('Standard Deviation: ', stdev, '\n'))
+text(5,6,cat('Confidence Interval, Lower: ', l, '\n'))
+text(5,5,cat('Confidence Interval, Upper: ', r, '\n'))
+
+
 
 # End PDF plotting device
 dev.off()
---
+++ fall2017/stoch_hydro/r_scripts/hw1/hw1_p3.r
@@ -0,0 +1,24 @@
+# Start PDF plotting device
+pdf('hw1_p3.pdf', height=5, width=5)
+
+# Null hypothesis: Means are the same
+# Alternative hypothesis: Means are different (downstream is greater)
+
+usgsdown.disch <- read.csv('usgs_08068740_daily_streamflow.csv')
+usgsup.disch <- read.csv('usgs_08068720_daily_streamflow.csv')
+
+# Downstream - Upstream > 0
+t.test(x=usgsdown.disch$flow_cfs[!is.na(usgsdown.disch$flow_cfs)],
+	y=usgsup.disch$flow_cfs[!is.na(usgsup.disch$flow_cfs)],
+	alternative='greater',var.equal=FALSE)
+
+# P-value is 0.078, meaning that we reject null hypothesis for 
+# alpha = 0.10 (90%), but we would NOT reject for alpha = 0.05 (95%).
+
+# Is this mean correct? Ignores NA values
+# mean <- mean(usgs.disch$flow_cfs,na.rm=TRUE)
+# stdev <- sd(usgs.disch$flow_cfs,na.rm=TRUE)
+# n <- length(usgs.disch$flow_cfs[!is.na(usgs.disch$flow_cfs)]) # Ignore NA values
+
+# End PDF plotting device
+dev.off()
