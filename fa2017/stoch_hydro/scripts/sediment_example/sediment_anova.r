eca388051c082ede670453bcaf2fda0f9a4e5dbe
Paul <pjruess@gmail.com>
2017-09-26 09:23:10 -0500

    Updating sediment example scripts

---
+++ fa2017/stoch_hydro/scripts/sediment_example/sediment_anova.r
@@ -0,0 +1,11 @@
+sed <- read.csv('Sediment_example.csv')
+
+sed$nMethod = as.numeric(sed$Method)
+
+# Method+Month uses variables only, with no interaction
+# Method*Month uses both variables and interaction
+# Method:Month uses only interaction
+sed.aov2 <- aov(SedimentTPD~Method*Month,data=sed)
+summary(sed.aov2)
+
+model.tables(sed.aov2)
