62366c6aec8cf45d953d21053929fa4f27ee31f5
Paul <pjruess@gmail.com>
2017-11-01 19:02:59 -0500

    Added miscellaneous grain storage research scripts

---
+++ research/grain_storage/vwc_zonal_stats/zonal_statistics.py
@@ -0,0 +1,53 @@
+import rasterstats
+import geopandas
+
+# import gdal, ogr, osr, numpy
+# import sys
+
+# Reference rasterstats: https://github.com/perrygeo/python-rasterstats
+# Reference rasterstats: http://www.perrygeo.com/index2.html
+# Reference rasterstats: http://www.perrygeo.com/python-rasterstats.html
+
+# Raster to tiff: https://gis.stackexchange.com/questions/54819/gdal-translate-converting-esri-grid-to-geotiff-in-batch
+# Read raster with GDAL: https://automating-gis-processes.github.io/2016/Lesson7-read-raster.html
+# Read ESRI gdb ideas: https://pcjericks.github.io/py-gdalogr-cookbook/vector_layers.html#get-all-layers-in-an-esri-file-geodatabase
+# Zonal statistics in straight-up python: https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html
+
+def zonal_stats_csv(polygon,raster,headers,filename):
+	stats = rasterstats.zonal_stats(
+		polygon,
+		raster,
+		stats='mean',
+		# copy_properties=True
+		# categorical=True
+		geojson_out=True
+		)#,stats='count min mean max median')
+
+	# Default encoding to utf-8 for odd string characters
+	import sys
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+
+	# Write to CSV
+	with open(filename,'w') as f:
+		f.write(','.join(headers))
+		f.write('\n')
+		for x in stats: 
+			p = x['properties']
+			s = [str(p[h]).encode('utf-8') for h in headers]
+			f.write(','.join(s))
+			f.write('\n')
+
+if __name__ == '__main__':
+	import os
+	polygon = '../cb_2016_us_county_500k/cb_2016_us_county_500k.shp'
+	headers = ['STATEFP','COUNTYFP','GEOID','NAME','ALAND','mean']
+	raster_names = [f for f in os.listdir('raw_tiffs/') if f.endswith('.tif')]
+	for r in raster_names:
+		filename = 'output/{0}.csv'.format(r.split('.')[0])
+		raster = 'raw_tiffs/{0}'.format(r)
+		zonal_stats_csv(
+			polygon=polygon,
+			raster=raster,
+			headers=headers,
+			filename=filename)
