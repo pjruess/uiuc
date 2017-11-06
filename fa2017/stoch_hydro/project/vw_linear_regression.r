14e13375c6fd778cd7cefe40132d9bbef7c725cc
Paul <pjruess@gmail.com>
2017-11-06 00:14:40 -0600

    First version of vw regression for mid-semester project update

--- fa2017/stoch_hydro/project/vw_linear_regression.r
+++ fa2017/stoch_hydro/project/vw_linear_regression.r
@@ -1,14 +1,3 @@
-# Dependent Variable: 
-# 	Virtual Water of Agricultural Production 
-# Independent Variables: 
-# 	Income
-# 	Population
-# 	On-Farm Grain Storage
-# 	Percent Developed Imperviousness (NLCD)
-# 	Precipitation
-
-
-
 ### Read in Data
 
 # Read in Production Survey Data (Corn, 2012)
@@ -121,62 +110,108 @@ if(!file.exists('linear_model_data.csv')){ # If file containing data summary doe
 }
 
 data <- data[complete.cases(data),]
+head(data)
 
+vars = c('YIELD','CWU','LANDAREA','INCOME','POPULATION','STORAGE')
+coms <- combn( vars, 2 )
+coms
+
+cat( 'ANOVA Test Results', file='anova_results.doc')
+print('Running ANOVA Tests...')
+for (i in seq( from=1, to=length(coms)/2) ) {
+	f <- paste( 'PRODUCTION~', paste( coms[1,i], coms[2,i], sep='*' ), sep='' )
+	print(f)
+	# Two-way ANOVA to test significance of variables
+	# H1: Mean (first-term) is equal for all counties
+	# H2: Mean (second-term) is equal for all counties
+	# H3: There is no interaction between (first-term) and (second-term)
+	data.aov <- aov( data=data, formula=as.formula(f) )
+	# cat('\n\n', file='anova_results.txt', append=TRUE)
+	cat( paste( '\nFormula: ', f,'\n'), file='anova_results.doc', append=TRUE )
+	capture.output(summary(data.aov), file='anova_results.doc', append=TRUE)
+	cat('\n', file='anova_results.doc', append=TRUE)
+	# print(summary(data.aov))
+}
+print('ANOVA Tests Completed.')
 # NORMALIZE OVER AREA
+
 
 # Define Linear Model
 library(car)
-LM <- lm(data=data,formula=PRODUCTION~INCOME+POPULATION+STORAGE+CWU+YIELD+LANDAREA)
-summary(LM)
+linear.model <- function(f,path){
+	print('Running Linear Model...')
+	print(f)
+	LM <<- lm(data=data,formula=as.formula(f)) # assign as global variable
+	cat('Linear Model Test Results\n', file=path)
+	cat( paste( 'Formula: ', f, '\n'), file=path, append=TRUE)
+	capture.output(summary(LM), file=path, append=TRUE)
+	print('Linear Model Created.')
+}
+
+# First Linear Model
+f = 'PRODUCTION~0+INCOME+STORAGE+CWU+YIELD+LANDAREA+YIELD:CWU+YIELD:INCOME+YIELD:STORAGE+CWU:LANDAREA+CWU:INCOME+CWU:STORAGE+LANDAREA:INCOME+INCOME:STORAGE'
+path = 'lm_results.doc'
+linear.model(f,path)
+
+# Define Linear Model, Edited
+f = 'PRODUCTION~0+INCOME+STORAGE+CWU+LANDAREA+YIELD:LANDAREA+CWU:INCOME+CWU:STORAGE+LANDAREA:INCOME'
+path='lm_edit_results.doc'
+linear.model(f,path)
+
+# Define Linear Model, Edited v2
+f = 'PRODUCTION~0+INCOME+STORAGE+CWU+LANDAREA+YIELD:LANDAREA+CWU:INCOME'
+path = 'lm_edit_v2_results.doc'
+linear.model(f,path)
+
+summary(LM) # final LM to be used
 
 # Plot Residuals
-vwc.hat <- fitted(LM)
-invisible(as.data.frame(vwc.hat))
-invisible(as.data.frame(data$VWC))
+production.hat <- fitted(LM)
+invisible(as.data.frame(production.hat))
+invisible(as.data.frame(data$PRODUCTION))
 
-pdf('residuals.pdf')
-plot(x=vwc.hat,y=data$VWC,xlab='VWC Model Results',ylab='VWC Raw Data',main='Residual Plot of VWC')
+png('residuals.png')
+plot(x=production.hat,y=data$PRODUCTION,xlab='PRODUCTION Model Results',ylab='PRODUCTION Raw Data',main='Residual Plot of PRODUCTION')
 abline(0,1,col='blue')
 dev.off()
 
 ### TEST FOR NORMALITY ###
 # Visualize Normality
-SEC.stdres <- rstandard(SEC) # standard residuals
-qqnorm(SEC.stdres)
-qqline(SEC.stdres)
+png('normality.png')
+LM.stdres <- rstandard(LM) # standard residuals
+qqnorm(LM.stdres)
+qqline(LM.stdres)
+dev.off()
 
 # Quantitative Check for Normality
 # Shapiro test... 
 # Ho: resid~N(0,sigma)
 # Ha: resid not normally distributed
-shapiro.test(SEC.stdres) # p < alpha --> do not reject null (resid normally dist)
+capture.output(shapiro.test(LM.stdres), file='shapiro_test.doc') # p < alpha --> do not reject null (resid normally dist)
 
 # Constant Variance
 # Visualize constant variance, manually
-SEC.res <- resid(SEC)
-plot(lit$RWM3D,SEC.res)
+png('manual_constvar.png')
+LM.res <- resid(LM)
+plot(data$PRODUCTION,LM.res)
 abline(0,0,lwd=1.5)
+dev.off()
 
 # Quantitative (and visual) check for Constant Variance
 # Tukey test... 
 # Ho: Quadratic term in trend line is zero... a*x^2 + b*x + c, a = 0
 # Ha: Quadratic term is not zero <-- heteroscedastic, a != 0
-residualPlots(SEC) # this returns resid plot for all variables, & Tukey p-values
-# Low p-values --> reject null --> heteroscedastic (unequal resid variance). BAD. 
-
-# Boxcox raises to lambda power to decrease heteroscedasticity
-boxcox(SEC) # determines lambda values
-
-# Independence (ie. lack of trends)
-# Can re-use residualPlots() to search for Indepedence
-# Autocorrelation: Lack of Independence
+png('residual_plots.png')
+capture.output(residualPlots(LM), file='tukey_test.doc') # this returns resid plot for all variables, & Tukey p-values
+dev.off()
 
 # Quantitative check for Independence
 # Durbin-Watson test...
 # Ho: Autocorrelation = 0
 # Ha: Autocorrelation != 0
 library(lmtest)
-dwtest(SEC,alternative='two.sided') # two.sided, greater, or less
+capture.output(dwtest(LM,alternative='two.sided'),file='dwtest.doc') # two.sided, greater, or less
 # Variance Inflation Factor
-vif(SEC) # Relationship between variables. Want low values; high means variables
+vif(LM)
+capture.output(vif(LM),'vif.doc') # Relationship between variables. Want low values; high means variables
 		 # are very related and are likely redundant
